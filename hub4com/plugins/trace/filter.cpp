b18ebf9a3e589efebd9a718a1a2ea0ad0447846d
vfrolov <>
2008-12-05 14:23:19 +0000

    Output config in loadable format

--- hub4com/plugins/trace/filter.cpp
+++ hub4com/plugins/trace/filter.cpp
@@ -19,6 +19,9 @@
  *
  *
  * $Log$
+ * Revision 1.11  2008/11/25 16:40:40  vfrolov
+ * Added assert for port handle
+ *
  * Revision 1.10  2008/11/24 12:37:00  vfrolov
  * Changed plugin API
  *
@@ -258,7 +261,8 @@ static HCONFIG CALLBACK ConfigStart()
                << "  {" << GetCommandLine() << "}" << endl;
 
   PrintTime(pConfig->buf);
-  pConfig->buf << "Config: {";
+  pConfig->buf << "Config:";
+  pConfig->buf << endl << "_BEGIN_";
 
   return (HCONFIG)pConfig;
 }
@@ -269,7 +273,7 @@ static BOOL CALLBACK Config(
 {
   _ASSERTE(hConfig != NULL);
 
-  ((TraceConfig *)hConfig)->buf << endl << "  {" << pArg << "}";
+  ((TraceConfig *)hConfig)->buf << endl << "  " << pArg;
 
   const char *pParam;
 
@@ -287,7 +291,7 @@ static void CALLBACK ConfigStop(
 {
   _ASSERTE(hConfig != NULL);
 
-  ((TraceConfig *)hConfig)->buf << endl << "}" << endl;
+  ((TraceConfig *)hConfig)->buf << endl << "_END_" << endl;
   ((TraceConfig *)hConfig)->PrintToAllTraceStreams(((TraceConfig *)hConfig)->buf.str().c_str());
 
   delete (TraceConfig *)hConfig;
