284345bc61b2c34ca3af7ce8d686c9b493384bc5
vfrolov <>
2012-06-04 06:15:37 +0000

    Fixed previous commit

--- com0com/setup/msg.cpp
+++ com0com/setup/msg.cpp
@@ -19,6 +19,9 @@
  *
  *
  * $Log$
+ * Revision 1.14  2012/06/04 06:02:14  vfrolov
+ * Added commented code with SetupSetNonInteractiveMode
+ *
  * Revision 1.13  2011/12/15 15:51:48  vfrolov
  * Fixed types
  *
